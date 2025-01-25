0f7f480b42216fac2793777ca60a0a04670d8a02
ankita007-coder <ankitabudhia42@gmail.com>
2025-01-25 17:59:21 +0530

    about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -70,8 +70,49 @@ const About = () => {
             Empowering Your Online Presence with Expert <br /> Web Development
             and Design Solutions.
           </p>
+          <div className="me-box">
+            <p>ANKITA BUDHIA</p>
+            <h3>Engineer by Day, Poet by Heart, Movie Buff by Night</h3>
+            <h4>A Little Bit of History</h4>
+            <p>
+              Since I can remember, I’ve always been drawn to stories—whether
+              told through poetry, books, or lines of code. My fascination with
+              technology began in the early days of rooting Android phones and
+              customizing ROMs. Watching technology evolve from the good ol’
+              Nokia days to today’s smartphones sparked a lifelong curiosity.{" "}
+              <br />
+              <br /> In 2019, I discovered the world of web development. A
+              simple YouTube video on HTML and CSS unlocked a passion for
+              building digital experiences. Over the years, I’ve sharpened my
+              skills in creating dynamic, scalable web applications, working
+              with clients and teams to bring their ideas to life.
+            </p>
+            <h4>The Journey of Code</h4>
+            <p>
+              As a web developer with frontend expertise, I combine creativity
+              with technical expertise to bring every project to life. With a
+              strong focus on user-friendly experiences and a keen eye for
+              design, I work closely with clients to fully understand their
+              needs and turn their vision into reality. My approach centers
+              around collaboration and clear communication, making sure to
+              explain technical details in simple terms. Whether I’m building a
+              brand-new website or enhancing an existing one, I always aim for
+              excellence in both aesthetics and functionality. experiences.
+            </p>
+            <p>
+              With a passion for staying ahead of the latest trends and
+              technologies, I am dedicated to delivering outstanding results
+              that exceed expectations. From freelancing gigs to professional
+              roles, my journey has been a blend of learning, creating, and
+              solving problems. My expertise lies in crafting intuitive
+              interfaces and robust backends, all while delivering exceptional
+              user
+            </p>
+          </div>
         </div>
-      </div>
+        
+        </div>
+
       <div className="others">
         <div className="steps-head">
           <h3>
